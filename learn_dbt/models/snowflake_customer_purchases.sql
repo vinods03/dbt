35fc20f9c2b11fe2cc1e57144a0143192a329cfd
vinods03 <vinods03@gmail.com>
2024-09-05 14:03:34 +0530

    dbt project

---
+++ learn_dbt/models/snowflake_customer_purchases.sql
@@ -0,0 +1,7 @@
+{{ config(materialized='table') }}
+
+select c.c_custkey, c.c_name, c.c_nationkey, sum(o.o_totalprice) as total_order_price
+from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER c
+left join SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS o
+on c.c_custkey = o.o_custkey
+group by c.c_custkey, c.c_name, c.c_nationkey
