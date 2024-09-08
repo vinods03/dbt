1220cd862b29e6cf106d9bad9ddc5be0e4560542
vinods03 <vinods03@gmail.com>
2024-09-08 09:58:32 +0530

    is it ok now

--- learn_dbt/models/example/my_first_dbt_model.sql
+++ learn_dbt/models/example/my_first_dbt_model.sql
@@ -14,9 +14,7 @@ with source_data as (
     select 1 as id, 'NY' as state, '2020-02-01 03:00:00.000'::timestamp as update_ts
     union all
     select 2 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as update_ts
-    union all
-    select null as id, 'TX' as state, '2020-01-01 00:00:00.000'::timestamp as update_ts
-
+  
 )
 
 select *
