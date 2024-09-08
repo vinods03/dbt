01f2e0f21006f85f84cc8b196f65ec7c95d5b084
vinods03 <vinods03@gmail.com>
2024-09-08 08:34:20 +0530

    fixing the bugs

--- learn_dbt/models/example/my_first_dbt_model.sql
+++ learn_dbt/models/example/my_first_dbt_model.sql
@@ -13,9 +13,7 @@ with source_data as (
 
     select 1 as id, 'NY' as state, '2020-02-01 03:00:00.000'::timestamp as update_ts
     union all
-    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as update_ts
-    union all
-    select 4 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as update_ts
+    select 2 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as update_ts
 
 )
 
